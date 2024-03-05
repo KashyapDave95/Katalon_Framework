eca3f2a661796443540a71628c5035c7ead0ccd7
Kashyap Dave <kashyap.dave@krishtechnolabs.local>
2024-03-05 13:26:56 +0530

    Added a home Test suite

---
+++ Test Suites/Homesearch.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Homesearch</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b36b6966-cd07-4f1d-a3e0-acbabcd2358b</testSuiteGuid>
+   <testCaseLink>
+      <guid>089536fb-4879-4110-99e7-b95471eb4ecc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/End-to-End/homepageSearch</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
